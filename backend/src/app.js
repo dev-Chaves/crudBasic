7e02c5ae1c07a53440b5682eb2f201e15a5765d8
Joao V Chaves <chavesprogrammer@gmail.com>
2024-12-18 16:48:50 -0300

    começando front

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const router = require('./router.js');
+
 app.use(express.json());
 
 app.use(router);
 
+app.use(cors());
 
 
 module.exports = app;
